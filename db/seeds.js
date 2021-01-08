5e8d3e37ade5c9da47f91e8c57457228778fc5fe
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-08 11:08:11 -0500

    edits

--- db/seeds.js
+++ db/seeds.js
@@ -6,10 +6,14 @@ const faker = require("faker")
 
  function seedUp(){
    let newUser = User.create({"name": "Will Jones", "email": "w@w.com", "password": "will"})
-     for(let i = 0; i < 100; i++){
-        let newUser = User.create({"name": faker.name.findName(), "email": faker.internet.email(), "password": faker.hacker.verb()})
-        let newStandUp = StandUp.create({"yesterday": faker.hacker.phrase(), "today": faker.hacker.phrase(), "blocker": faker.hacker.phrase(), "userId": Math.floor(Math.random() * 101)  })
-     }
+   
+    for(let i = 0; i < 10; i++){   
+      let newUser = User.create({"name": faker.name.findName(), "email": faker.internet.email(), "password": faker.hacker.verb()})
+    }
+
+    // for(let i = 0; i < 50; i++){
+    //   let newStandUp = StandUp.create({"yesterday": faker.hacker.phrase(), "today": faker.hacker.phrase(), "blocker": faker.hacker.phrase(), "userId": Math.floor(Math.random() * 11)  })
+    // }
 }
 
 
