7f8a411580d5a1492dfca8e24435c95ea5d5e638
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-10 18:16:26 -0500

    configured for local

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,24 +1,21 @@
 const Sequelize = require("sequelize");
 const StandUp = require("../models/standUp");
 
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
-    protocol: 'postgres',
-    port:     '5432',
-    host:     'ec2-23-23-88-216.compute-1.amazonaws.com',
-    logging:  true //false
-  })
-} else {
-  // the application is executed on the local machine ... use mysql
-
+// if (process.env.DATABASE_URL) {
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect:  'postgres',
+//     protocol: 'postgres',
+//     port:     '5432',
+//     host:     'ec2-23-23-88-216.compute-1.amazonaws.com',
+//     logging:  true //false
+//   })
+// } else {
   const sequelize = new Sequelize('standUp_DB', 'root', 'password', {
     host: 'localhost',
     dialect: 'mysql',
   })
 
-}
+// }
 
 
 
