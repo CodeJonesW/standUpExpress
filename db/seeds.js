bcf6f028ac53e54e567e8524a7faa1cb3f542ace
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-10 14:30:02 -0500

    seeds

--- db/seeds.js
+++ db/seeds.js
@@ -16,7 +16,7 @@ const faker = require("faker")
 }
 
   async function seedStandUps(){
-      for(let i = 1; i < 3; i++){
+      for(let i = 1; i < 10; i++){
       await StandUp.create({"yesterday": faker.hacker.phrase(), "today": faker.hacker.phrase(), "blocker": faker.hacker.phrase(), "userId": "1"  })
       await StandUp.create({"yesterday": faker.hacker.phrase(), "today": faker.hacker.phrase(), "blocker": faker.hacker.phrase(), "userId": "2"  })
       await StandUp.create({"yesterday": faker.hacker.phrase(), "today": faker.hacker.phrase(), "blocker": faker.hacker.phrase(), "userId": "3"  })
