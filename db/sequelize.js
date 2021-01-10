377f91f2a0c77bae73050d4b75ba267c586a8102
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-10 18:23:11 -0500

    updated dynamic for local and heroku

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,21 +1,22 @@
 const Sequelize = require("sequelize");
 const StandUp = require("../models/standUp");
 
-// if (process.env.DATABASE_URL) {
-//   sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect:  'postgres',
-//     protocol: 'postgres',
-//     port:     '5432',
-//     host:     'ec2-23-23-88-216.compute-1.amazonaws.com',
-//     logging:  true //false
-//   })
-// } else {
-  const sequelize = new Sequelize('standUp_DB', 'root', 'password', {
+let sequelize
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     '5432',
+    host:     'ec2-23-23-88-216.compute-1.amazonaws.com',
+    logging:  true //false
+  })
+} else {
+  sequelize = new Sequelize('standUp_DB', 'root', 'password', {
     host: 'localhost',
     dialect: 'mysql',
   })
 
-// }
+}
 
 
 
