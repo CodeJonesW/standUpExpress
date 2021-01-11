6d88ae41cba6162b03c6d33d7000648721d0081e
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-10 19:00:12 -0500

    res header cors

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ const app = express();
 app.use(function(req, res, next) {
     
         res.header('Access-Control-Allow-Origin', '*');
+        res.header("Access-Control-Allow-Credentials", true);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
     
     next();
 });
