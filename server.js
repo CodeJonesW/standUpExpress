06f1780eff2886797124bf582f745760842d450e
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-10 19:16:25 -0500

    if optins is req method send 200

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
     
-    next();
+
+        if ('OPTIONS' == req.method) {
+            res.send(200);
+          } else {
+            next();
+          }
 });
 
 app.use(logger("dev"));
